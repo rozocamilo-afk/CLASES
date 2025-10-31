14bcff85503d6d489c9ff76bc6c67c709657efb3
Juan Camilo Rozo Mora <rozocamilo@gmail.com>
2025-10-31 02:10:43 -0500

    Taller Juan-developer

--- Clase-13/mi-app-react/src/App.jsx
+++ Clase-13/mi-app-react/src/App.jsx
@@ -1,14 +1,26 @@
 // src/App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Contador from './components/Contador.jsx';
+// Importamos nuestros componentes de página
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Perfil from './components/Perfil';
+import Detalles from './components/Detalles';
 
 function App() {
   return (
-    <>
-      <h1>Mi Primer Componente en React</h1>
-      <Contador />
-    </>
-  )
+    <BrowserRouter>
+      {/* La barra de navegación estará visible en TODAS las páginas */}
+      <Navbar />
+
+      {/* Aquí definimos qué componente mostrar para cada ruta */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/perfil/:id" element={<Perfil />} />
+        <Route path="/detalles/:proyecto" element={<Detalles />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
